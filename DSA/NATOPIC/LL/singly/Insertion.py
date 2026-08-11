42aebc22e1755f1e7d2402e67ad2d14f209aed16
Protyush <ghorui.protyushraj@gmail.com>
2026-08-11 09:45:22 +0530

    Insertion done(all 3 parts)

--- DSA/NATOPIC/LL/singly/Insertion.py
+++ DSA/NATOPIC/LL/singly/Insertion.py
@@ -18,8 +18,8 @@ class LinkedList:
             current = current.next
 
         print("None")
-
     # Insert at Head
+    # --------------------------------
     def insert_head(self, val):
         new_node = ListNode(val)
 
@@ -42,24 +42,66 @@ class LinkedList:
 
         current.next = new_node
 
+    # Insert at Position
+    def insert_at_position(self, val, position):
+
+        # Invalid position
+        if position < 0:
+            print("Invalid position")
+            return
+
+        # Position 0 = Insert at Head
+        if position == 0:
+            self.insert_head(val)
+            return
+
+        new_node = ListNode(val)
+
+        current = self.head
+
+        # Reach the node BEFORE the position
+        for _ in range(position - 1):
+
+            if current is None:
+                print("Invalid position")
+                return
+
+            current = current.next
+
+        # Position is outside the list
+        if current is None:
+            print("Invalid position")
+            return
+
+        # Connect new node
+        new_node.next = current.next
+        current.next = new_node
+
 
 # Use the Linked List
 
 ll = LinkedList()
 
 
-# Insert at head
+# Insert at Head
 ll.insert_head(1)
 ll.insert_head(2)
 ll.insert_head(3)
 
 ll.display()
-# 3 → 2 → 1 → None
+# 3 -> 2 -> 1 -> None
 
 
-# Insert at tail
+# Insert at Tail
 ll.insert_tail(4)
 ll.insert_tail(5)
 
 ll.display()
 # 3 -> 2 -> 1 -> 4 -> 5 -> None
+
+
+# Insert at Position
+ll.insert_at_position(10, 2)
+
+ll.display()
+# 3 -> 2 -> 10 -> 1 -> 4 -> 5 -> None
